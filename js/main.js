abf951faf40fc2fc6ad47fdd993efb4de591d94f
taiwo oluwole-young <taiwooluwoleyoung@gmail.com>
2023-02-17 14:50:46 +0100

    made the buttons reactive, changes to bg-color

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ let toggleBtn = document.querySelector('.togglebtn');
 let closeBtn = document.querySelector('.closebtn');
 let sideBar = document.querySelector('.side-bar');
 
-
 toggleBtn.addEventListener('click', function(){
+    sideBar.classList.remove('displayOff');
     sideBar.classList.toggle('displayOn');
 });
 closeBtn.addEventListener('click', function(){
     sideBar.classList.remove('displayOn');
+    sideBar.classList.add('displayOff');
 });
